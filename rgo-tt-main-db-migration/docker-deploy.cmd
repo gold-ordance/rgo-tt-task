04a6aeb9e49b00de439a61c6add2a1d63e73a9be
evgeny-mordyasov <michisig11i7@gmail.com>
2023-09-16 19:45:33 +0700

    fix db migration

--- rgo-tt-main-db-migration/docker-deploy.cmd
+++ rgo-tt-main-db-migration/docker-deploy.cmd
@@ -1,5 +1,6 @@
 docker-compose up -d
 docker rmi michisig/tt-task-db:%1
-docker container commit native-db michisig/tt-task-db:%1
+docker container commit task-db michisig/tt-task-db:%1
 docker image push michisig/tt-task-db:%1
 docker-compose stop
+docker rmi --force task-db:latest
