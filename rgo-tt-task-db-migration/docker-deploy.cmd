59da99093bd2f5837eeada9fecec81c3cb15051e
evgeny-mordyasov <michisig11i7@gmail.com>
2023-09-17 12:16:57 +0700

    fix logback.xml files

--- rgo-tt-task-db-migration/docker-deploy.cmd
+++ rgo-tt-task-db-migration/docker-deploy.cmd
@@ -3,4 +3,5 @@ docker rmi michisig/tt-task-db:%1
 docker container commit task-db michisig/tt-task-db:%1
 docker image push michisig/tt-task-db:%1
 docker-compose stop
-docker rmi --force task-db:latest
+docker rm task-db
+docker rmi task-db:latest
